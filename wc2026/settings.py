af488048b6b303e3c2db45c8c684e04c1b04268d
Onorio de Jesus <onorio@catalpa.io>
2026-06-22 16:59:04 +0900

    database configuration is missing

--- wc2026/settings.py
+++ wc2026/settings.py
@@ -37,7 +37,12 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "wc2026.wsgi.application"
 
-DATABASES = {}
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
+}
 
 LANGUAGE_CODE = "en-us"
 TIME_ZONE = "UTC"
