c70060c95910f423183653c0a4a48cc9d78660b1
Onorio de Jesus <onorio@catalpa.io>
2026-06-22 17:02:20 +0900

    added django.contrib.auth is missing

--- wc2026/settings.py
+++ wc2026/settings.py
@@ -9,6 +9,8 @@ DEBUG = True
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "kopamundial.pythonanywhere.com"]
 
 INSTALLED_APPS = [
+    "django.contrib.admin",
+    "django.contrib.auth",
     "django.contrib.contenttypes",
     "django.contrib.staticfiles",
     "bracket",
